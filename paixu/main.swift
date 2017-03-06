18bf4ae94db9aee7a9ee49b158df26fbbe16d1ed
20151104705 <lwbJY666>
2017-03-06 10:38:13 +0800

    diyici

---
+++ paixu/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  paixu
+//
+//  Created by 20151104705 on 2017/3/6.
+//  Copyright © 2017年 20151104705. All rights reserved.
+//
+
+import Foundation
+
+var array = String()
+for index in 1...10{
+    array.append(" \(index)")
+}
+print (array)
+
